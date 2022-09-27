2b1b1dcf8e5a5a53ce85219f60601fece46e7a41
ZTS3998a <ZTS3998a@u2-209-14.ens.fsi>
2022-09-27 19:59:00 +0200

    01

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,17 @@
+package personnages;
+
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1 ;
+	
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+
+	
+	
+}
