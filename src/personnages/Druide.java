590caec173da5d57a09986da6c2bc7b26d7aac85
islamu <114508095+islamu@users.noreply.github.com>
2022-11-15 16:25:31 +0100

    Update Druide.java

--- src/personnages/Druide.java
+++ src/personnages/Druide.java
@@ -1,7 +1,10 @@
 package personnages;
+import java.util.Random ; 
+
 
 public class Druide {
 	private String nom;
+	private int forcePotion =1 ;
 	private int effetPotionMin;
 	private int effetPotionMax;
 
@@ -9,7 +12,7 @@ public class Druide {
 		this.nom = nom;
 		this.effetPotionMin = effetPotionMin;
 		this.effetPotionMax = effetPotionMax;
-		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " à "
+		parler("Bonjour, je suis le druide " + nom + " et ma potion peut aller d'une force " + effetPotionMin + " Ã  "
 				+ effetPotionMax + ".");
 	}
 
@@ -18,11 +21,40 @@ public class Druide {
 	}
 
 	public void parler(String texte) {
-		System.out.println(prendreParole() + "« " + texte + "»");
+		System.out.println(prendreParole() + "Â« " + texte + "Â»");
 	}
 
 	private String prendreParole() {
 		return "Le druide " + nom + " : ";
 	}
-   
-}
+	public void preparerPotion() {
+		Random random = new Random();
+		forcePotion = random.nextInt(effetPotionMax - effetPotionMin +1 );
+		forcePotion += effetPotionMin ; 
+		faireParler();
+	}
+
+	public void faireParler() {
+		if (forcePotion > 7 ) {
+			parler("j'ai prepare une super potion de force :" + forcePotion);
+		}
+		else {
+			parler("je n'ai pas trouve tous les ingridients , ma potion est seulement de force : "+ forcePotion); 
+		}
+		}
+	public void booster(Gaulois gaulois) {
+		String nomGaulois = gaulois.getNom(); 
+		if ("Obelix".equals(nomGaulois)) {
+			parler("Non, ObÃ©lix !... Tu nâ€™auras pas de potion magique !");
+		}
+		else {
+			gaulois.boirePotion(forcePotion);
+		}
+	}
+	public static void main(String[] args) {
+		Druide panoramix = new Druide("Panoramix",5,10); 
+		panoramix.preparerPotion();
+	}
+	
+		
+	}
