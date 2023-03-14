95c0da53a8c6a78726e7e8fc0903c5c5d251dbd4
islamu <114508095+islamu@users.noreply.github.com>
2023-03-14 09:44:01 +0100

    Update Scenario.java

--- src/Histoire/Scenario.java
+++ src/Histoire/Scenario.java
@@ -1,23 +1,34 @@
 package histoire;
+
+import personnages.Druide;
+import personnages.Equipement;
 import personnages.Gaulois;
 import personnages.Romain;
-import personnages.Druide;
+import villagegaulois.Musee;
 
 public class Scenario {
-	public static void main(string[] args) {
-		Gaulois asterix = new Gaulois("Asterix",8);
-		Gaulois obelix = new Gaulois("Obelix",8);
-		Romain minus = new Romain("Minus",6);
-		Druide panoramix = new Druide("Panoramix",5,10);
-		panoramix.parler("je vais aller preparer une petite potion...");
-		panoramix.preparerPotion();
-		panoramix.booster(obelix); 
-		obelix.parler("Par Belenos, ce n'est pas juste !");
-		panoramix.booster(asterix);
+
+	public static void main(String[] args) {
+		Druide druide = new Druide("Panoramix", 5, 10);
+		druide.parler("Je vais aller préparer une petite potion...");
+		druide.preparerPotion();
+		Gaulois obelix = new Gaulois("Obélix", 25);
+		Gaulois asterix = new Gaulois("Astérix", 8);
+		druide.booster(obelix);
+		obelix.parler("Par Bélénos, ce n'est pas juste !");
+		druide.booster(asterix);
 		asterix.parler("Bonjour");
-		minus.parler("UN GAU...UN GAUGAU...");
-		while (minus.getForce()>0) {
+		Romain minus = new Romain("Minus", 6);
+		Romain milexcus = new Romain("Milexcus", 8);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		milexcus.sEquiper(Equipement.CASQUE);
+		minus.parler("UN GAU... UN GAUGAU...");
+		do {
 			asterix.frapper(minus);
-		}
-	}
-}
+		} while (minus.getForce() > 0);
+		milexcus.parler("UN GAU... UN GAUGAU...");
+		do {
+			asterix.frapper(milexcus);
+		} while (milexcus.getForce() > 0);
+	}}
